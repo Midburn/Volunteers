7722adae3e8468301486b81402c773d90ade1d87
eyalliebermann <eyal.liebermann@gmail.com>
2017-03-15 20:45:58 +0200

    export search and add constructor

--- src/components/FilterComponent/FilterComponent.js
+++ src/components/FilterComponent/FilterComponent.js
@@ -1,12 +1,16 @@
 var React = require('react');
-var SearchFilter = require('../SearchFilter/SearchFilter');
+//  var SearchFilter = require('../SearchFilter/SearchFilter');
 var DropdownFilter= require('../DropdownFilter/DropdownFilter');
+
+import  SearchFilter  from '../SearchFilter/SearchFilter.js';
 
 module.exports = React.createClass({
     render: function() {
+        console.log('FilterComponent search import');
+        console.log(SearchFilter);
         return (
             <div className="filter-component">
-                <SearchFilter/>
+                <SearchFilter filterText='a'/>
                 <DropdownFilter column="Department"/>
                 <DropdownFilter column="Volunteer Type"/>
                 <DropdownFilter column="Got Ticket"/>
--- src/components/SearchFilter/SearchFilter.js
+++ src/components/SearchFilter/SearchFilter.js
@@ -1,12 +1,26 @@
 var React = require('react');
 
-module.exports = React.createClass({
-    render: function() {
+export default class SearchFilter extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+  }
+
+   handleFilterTextInputChange(e) {
+  //  this.props.onFilterTextInput(e.target.value);
+  }
+    render() {
         return (
             <div className="filter-component form-group">
                 <label for="search_name">Search</label>
-                <input type="text" id="search_name" className="form-control" placeholder="Search by user's first name, last name or email" />
+                <input 
+                    type="text" 
+                    id="search_name" 
+                    value={this.props.filterText}
+                    onChange={this.handleFilterTextInputChange}
+                    className="form-control" 
+                    placeholder="Search by user's first name, last name or email" />
             </div>
         );
     }
-});
+}
