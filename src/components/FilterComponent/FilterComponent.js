244aa6aa9c0a97677f80212d66f86ebf6300337d
eyalliebermann <eyal.liebermann@gmail.com>
2017-03-16 00:55:03 +0200

    bootstrap grid system pre-functional

--- src/components/FilterComponent/FilterComponent.js
+++ src/components/FilterComponent/FilterComponent.js
@@ -9,12 +9,12 @@ module.exports = React.createClass({
         console.log('FilterComponent search import');
         console.log(SearchFilter);
         return (
-            <div className="filter-component">
-                <SearchFilter filterText={this.props.filters.filterText} onFilterTextInput={this.props.onFilterTextInput}/>
-                <DropdownFilter label="Department" options={['All','Tech','Navadim','Mapatz','Tnua','Merkazia']}/>
-                <DropdownFilter label="Volunteer Type" options={['All','Manager','Day Manager','Shift Manager','Production','Department Manager']}/>
-                <DropdownFilter label="Got Ticket" options={['All','Yes','No']}/>
-                <DropdownFilter label="Production" options={['All','Yes','No']}/>
+            <div className="filter-component row">
+                <SearchFilter className="col-md-12" filterText={this.props.filters.filterText} onFilterTextInput={this.props.onFilterTextInput}/>
+                <DropdownFilter className="col-md-4" label="Department" options={['All','Tech','Navadim','Mapatz','Tnua','Merkazia']}/>
+                <DropdownFilter className="col-md-4" label="Volunteer Type" options={['All','Manager','Day Manager','Shift Manager','Production','Department Manager']}/>
+                <DropdownFilter lassName="col-md-4" label="Got Ticket" options={['All','Yes','No']}/>
+                <DropdownFilter lassName="col-md-4" label="Production" options={['All','Yes','No']}/>
              </div>
         );
     }
