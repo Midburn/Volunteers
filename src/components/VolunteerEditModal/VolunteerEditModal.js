15827afb591e79b8098e8d9fd65adef3fa148564
eyalliebermann <eyal.liebermann@gmail.com>
2017-03-19 18:13:27 +0200

    binding is_production to track changes

--- src/components/VolunteerEditModal/VolunteerEditModal.js
+++ src/components/VolunteerEditModal/VolunteerEditModal.js
@@ -10,81 +10,106 @@ export default class VolunteerEditModal extends React.Component{
         this.state={};
         this.handleCancel=this.handleCancel.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
+        this.handleReset=this.handleReset.bind(this);
+
+        this.handleChange=this.handleChangeProduction.bind(this);
+        this.calcValueProduction= this.calcValueProduction.bind(this);
+        this.handleChage=this.handleChange.bind(this);
     }
+
+    handleChange(e){
+        consle.log(e);
+    }
+
+    calcValueProduction(){
+        return (this.state.is_production!=undefined? this.state.is_production: this.props.volunteer.is_production)?
+            'Yes':
+            'No';
+    }
+
+    handleChangeProduction(e){
+        console.log('handle change production');
+        console.log(e);
+        this.setState( {is_production: e.target.value} );
+    }
+
+    
     handleCancel(){
         this.props.onHide();
     }
     handleSubmit(){
         this.props.onHide();
     }
+    handleReset(){
+        //TODO clear state
+    }
     render(){
-                console.log('Modal Rended');
-
+        console.log('Modal Rended');
         console.log(this.props);
 
         return (
-         <Modal show={this.props.show} onHide={this.handleCancel}>
-          <Modal.Header closeButton>
-            <Modal.Title>Editing {this.props.volunteer.first_name} {this.props.volunteer.last_name}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-           
+            <Modal show={this.props.show} onHide={this.handleCancel}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Editing {this.props.volunteer.first_name} {this.props.volunteer.last_name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                     <form>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-form-label">First Name</label>
+                            <div className="col-sm-8">
+                                <p className="form-control-static">{this.props.volunteer.first_name}</p>
+                            </div>
+                        </div>
+            
+                    <div className="form-group row">
+                        <label className="col-sm-4 col-form-label">Last Name</label>
+                        <div className="col-sm-8">
+                        <p className="form-control-static">{this.props.volunteer.last_name}</p>
+                        </div>
+                    </div>
+            
+                    <div className="form-group row">
+                        <label className="col-sm-4 col-form-label">Email</label>
+                        <div className="col-sm-8">
+                        <p className="form-control-static">{this.props.volunteer.email}</p>
+                        </div>
+                    </div>
 
-            <form>
-            <div className="form-group row">
-                <label className="col-sm-4 col-form-label">First Name</label>
-                <div className="col-sm-8">
-                    <p className="form-control-static">{this.props.volunteer.first_name}</p>
-                </div>
-            </div>
-            <div className="form-group row">
-                <label className="col-sm-4 col-form-label">Last Name</label>
-                <div className="col-sm-8">
-                <p className="form-control-static">{this.props.volunteer.last_name}</p>
-                </div>
-            </div>
-            <div className="form-group row">
-                <label className="col-sm-4 col-form-label">Email</label>
-                <div className="col-sm-8">
-                <p className="form-control-static">{this.props.volunteer.email}</p>
-                </div>
-            </div>
+                    <div className="form-group row">
+                        <label for="Role" className="col-sm-4 col-form-label">Role</label>
+                        <div className="col-sm-10">
+                            <select
+                                onChange ={this.handleChange}
+                                value="Manager"
+                                className="form-control" 
+                                id="Role">
+                                {
+                                    ['All','Manager','Day Manager','Shift Manager','Production','Department Manager'].map(
+                                    (option)=> <option value={option} key={option}>{option}</option>
+                                    )    
+                                }
+                            </select>
+                        </div>
+                    </div>
 
-            <div className="form-group row">
-                <label for="Role" className="col-sm-4 col-form-label">Role</label>
-                <div className="col-sm-10">
-                    <select
-                      onChange ={()=>false}
-                value="Manager"
-                                    className="form-control" 
-                    id="Role">
-                        {
-                            ['All','Manager','Day Manager','Shift Manager','Production','Department Manager'].map(
-                            (option)=> <option value={option} key={option}>{option}</option>
-                            )    
-                        }
-                    </select>
-                </div>
-            </div>
-
-            <div className="form-group row">
-                <label for="Production" className="col-sm-4 col-form-label">Production</label>
-                <div className="col-sm-10">
-                    <select
-                      onChange ={()=>false}
-                value={this.props.volunteer.is_production?'Yes':'No'}
-                                    className="form-control" 
-                    id="Production">
-                        {
-                            ['Yes','No'].map(
-                            (option)=> <option value={option} key={option}>{option}</option>
-                            )    
-                        }
-                    </select>
-                </div>
-            </div>
-            </form>
-        </Modal.Body>
+                    <div className="form-group row">
+                        <label for="Production" className="col-sm-4 col-form-label">Production</label>
+                        <div className="col-sm-10">
+                            <select
+                                onChange ={this.handleChangeProduction}
+                                value={this.calcValueProduction()}
+                                className="form-control" 
+                                id="Production">
+                                {
+                                    ['Yes','No'].map(
+                                    (option)=> <option value={option} key={option}>{option}</option>
+                                    )    
+                                }
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.handleCancel}>Cancel</Button>
             <Button onClick={this.handleReset}>Reset</Button>
