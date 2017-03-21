2115e4704335e3abbdd94e3973355907fff079b2
eyalliebermann <eyal.liebermann@gmail.com>
2017-03-21 18:06:20 +0200

    factor out bollean and all conversion

---
+++ src/DropdownConverter.js
@@ -0,0 +1,22 @@
+
+export default class Converter{
+    constructor(){}
+
+     convertFromDisplay(displayValue){
+        switch(displayValue){   
+            case 'All': return null;
+            case 'Yes': return true;
+            case 'No': return false;
+            default: return displayValue;
+        }
+    }
+
+    convertToDisplay(logicValue){
+        switch(logicValue){   
+            case null: return 'All';
+            case true: return 'Yes';
+            case false: return 'No';
+            default: return logicValue;
+        }
+    }
+)
