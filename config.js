99d57671cb7d23746b86d19aaf159703af4029fc
shaytizer <shay@intezer.com>
2017-04-02 21:58:18 +0300

    change default env to debug

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.environment = process.env.NODE_ENV || 'production' // [debug | production]
+config.environment = process.env.NODE_ENV || 'debug' // [debug | production]
 
 config.port = process.env.PORT || 8080
 
