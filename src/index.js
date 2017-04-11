aabe486963158024c5a5431d057f42ec102ffc08
Guy Warburg <guy.warburg@gmail.com>
2017-04-11 21:22:24 +0300

    refactor and add try/catch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import { AppContainer } from 'react-hot-loader';
 
@@ -7,13 +8,13 @@ import App from './routes/App'
 import ComingSoon from './components/ComingSoon/ComingSoon'
 
 const fetchUserData = async function() {
-    const resp = await fetch('/api/v1/volunteers/me', {credentials: 'include'});
-    const me = await resp.json();
-    console.log(JSON.stringify(me));
-    if (me.length > 0) {
-        render(App);
-    } else {
-        render(ComingSoon);
+    try {
+      const resp = await axios.get('/api/v1/volunteers/me', {credentials: 'include'});
+      console.log(JSON.stringify(resp));
+      render(App);
+    } catch(error) {
+      console.log(error);
+      render(ComingSoon);
     }
 };
 
