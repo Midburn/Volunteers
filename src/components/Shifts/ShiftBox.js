3074afb6fb18c814bf8ec0443a22a34fbd48d1a5
May Ben Arie <may22989@gmail.com>
2017-04-23 21:22:23 +0300

    Shifts - change id to _id

--- src/components/Shifts/ShiftBox.js
+++ src/components/Shifts/ShiftBox.js
@@ -8,18 +8,18 @@ require("moment-duration-format")
 const heightPerMinute = 1.5
 
 const ShiftBox = observer(({shift, onEdit, onDelete, focusedShift, onFocus}) => {
-    const {id, title, color, startDate, endDate, volunteers} = shift
+    const {_id, title, color, startDate, endDate, volunteers} = shift
     const startOfDay = moment(startDate).startOf('day')
     const relStart = moment(startDate).diff(startOfDay, 'days', true)
     const relEnd = moment(endDate).diff(startOfDay, 'days', true)
-    return <div id={id} key={id} className={`shift-box ${shift.id === focusedShift ? 'focused' :''}`}
+    return <div id={_id} key={_id} className={`shift-box ${shift._id === focusedShift ? 'focused' :''}`}
             style={{ 
                 left: `${shift.overlapIndex * 100 / shift.overlapCount}%`,
                 top: `${relStart * 100}%`,
                 width: `${100 / shift.overlapCount}%`,
                 height: 0
                 }}>
-                <div className="box" style={{backgroundColor: color, height: moment(endDate).diff(startDate, 'minutes') * heightPerMinute }}  onClick={() => onFocus(id)}>
+                <div className="box" style={{backgroundColor: color, height: moment(endDate).diff(startDate, 'minutes') * heightPerMinute }}  onClick={() => onFocus(_id)}>
                     <div className="title">{title}</div>
                     <div className="duration">{moment(startDate).format('H:mm')} - {moment(endDate).format('H:mm')}</div>
                     <div className="count">{volunteers.length} volunteers</div>
