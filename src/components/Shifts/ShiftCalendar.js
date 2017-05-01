6f8cbd0058856d83f9ede132c8119880656c1c24
shaytizer <shay@intezer.com>
2017-05-01 17:03:50 +0300

    Feature(shofts): Enable calender selection

--- src/components/Shifts/ShiftCalendar.js
+++ src/components/Shifts/ShiftCalendar.js
@@ -8,36 +8,51 @@ import {Button} from 'react-bootstrap'
 import BigCalendar from 'react-big-calendar';
 require('react-big-calendar/lib/css/react-big-calendar.css');
 BigCalendar.setLocalizer(
-  BigCalendar.momentLocalizer(moment)
+    BigCalendar.momentLocalizer(moment)
 );
 
 const Event = ({event: {shift, onEdit, onDelete}}) =>
-	<span>
+    <span>
 		<strong>
 			{shift.title}
 		</strong>
 		<p>	{shift.volunteers.length} volunteers</p>
 		<div className="toolbar">
-			<Button bsSize="xsmall" onClick={() => onEdit(shift)} className="glyphicon glyphicon-edit" />
-			<Button bsSize="xsmall" onClick={() => onDelete(shift)} className="glyphicon glyphicon-trash" />
+			<Button bsSize="xsmall" onClick={() => onEdit(shift)} className="glyphicon glyphicon-edit"/>
+			<Button bsSize="xsmall" onClick={() => onDelete(shift)} className="glyphicon glyphicon-trash"/>
 		</div>
 	</span>
 
 const views = ['week', 'day']
-export default observer(({shiftManagerModel: { date, filteredShifts, weekView, editShift, deleteShift}}) => (
-	<BigCalendar
-		date={moment(date)}
-		view={weekView ? 'week' : 'day'}
-		toolbar={false}
-		components={{
-			event: Event
-		}}
-		events={_.toArray(filteredShifts).map((shift) => ({
-			start: new Date(shift.startDate),
-			end: new Date(shift.endDate),
-			shift,
-			onEdit: editShift,
-			onDelete: deleteShift
-		}))}
-	/>
+export default observer(({
+    shiftManagerModel: {
+        date,
+        filteredShifts,
+        weekView,
+        editShift,
+        deleteShift,
+        createShift
+    }
+}) => (
+    <BigCalendar
+        date={moment(date).toDate()}
+        onNavigate={()=>{}}
+        view={weekView ? 'week' : 'day'}
+        onView={()=>{}}
+        toolbar={false}
+        selectable={true}
+        components={{
+            event: Event
+        }}
+        onSelectSlot={slotInfo => {
+            createShift(slotInfo.start, slotInfo.end);
+        }}
+        events={_.toArray(filteredShifts).map((shift) => ({
+            start: new Date(shift.startDate),
+            end: new Date(shift.endDate),
+            shift,
+            onEdit: editShift,
+            onDelete: deleteShift
+        }))}
+    />
 ));
