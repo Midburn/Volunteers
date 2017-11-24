8de0c1c8b0c1845e1eec66f22dd55b31ed0056c1
shay <shay@intezer.com>
2017-11-24 22:59:53 +0200

    feature(volunteer request) - create model, routes and request tab

--- src/routes/App.js
+++ src/routes/App.js
@@ -6,93 +6,98 @@ import TimeClock from "./TimeClock/TimeClock";
 import VolunteerShifts from "../components/VolunteerShifts/VolunteerShifts";
 import Header from "../components/Header/Header";
 import ComingSoon from "../components/ComingSoon/ComingSoon";
+import VolunteerRequest from "../components/VolunteerRequest/VolunteerRequest";
 
 const routesConfig = {
-  '1': {
-    routes: [
-      {
-        path: '/volunteer-list-tab',
-        component: VolunteerListTab
-      },
-      {
-        path: '/shift-manager',
-        component: ShiftManager
-      },
-      {
-        path: '/time-clock',
-        component: TimeClock
-      },
-      {
-        path: '/my-shifts',
-        component: VolunteerShifts
-      }
-    ],
-    defaultComponent: VolunteerListTab
-  },
-  '2': {
-    routes: [
-      {
-        path: '/volunteer-list-tab',
-        component: VolunteerListTab
-      },
-      {
-        path: '/shift-manager',
-        component: ShiftManager
-      },
-      {
-        path: '/time-clock',
-        component: TimeClock
-      },
-      {
-        path: '/my-shifts',
-        component: VolunteerShifts
-      }
-    ],
-    defaultComponent: VolunteerListTab
-  },
-  '3': {
-    routes: [
-      {
-        path: '/shift-manager',
-        component: ShiftManager
-      },
-      {
-        path: '/time-clock',
-        component: TimeClock
-      },
-      {
-        path: '/my-shifts',
-        component: VolunteerShifts
-      }
-    ],
-    defaultComponent: VolunteerShifts
-  }
+    '1': {
+        routes: [
+            {
+                path: '/volunteer-list-tab',
+                component: VolunteerListTab
+            },
+            {
+                path: '/shift-manager',
+                component: ShiftManager
+            },
+            {
+                path: '/time-clock',
+                component: TimeClock
+            },
+            {
+                path: '/my-shifts',
+                component: VolunteerShifts
+            },
+            {
+                path: "/volunteer-requests",
+                component: VolunteerRequest
+            }
+        ],
+        defaultComponent: VolunteerListTab
+    },
+    '2': {
+        routes: [
+            {
+                path: '/volunteer-list-tab',
+                component: VolunteerListTab
+            },
+            {
+                path: '/shift-manager',
+                component: ShiftManager
+            },
+            {
+                path: '/time-clock',
+                component: TimeClock
+            },
+            {
+                path: '/my-shifts',
+                component: VolunteerShifts
+            }
+        ],
+        defaultComponent: VolunteerListTab
+    },
+    '3': {
+        routes: [
+            {
+                path: '/shift-manager',
+                component: ShiftManager
+            },
+            {
+                path: '/time-clock',
+                component: TimeClock
+            },
+            {
+                path: '/my-shifts',
+                component: VolunteerShifts
+            }
+        ],
+        defaultComponent: VolunteerShifts
+    }
 };
 
 class App extends React.Component {
 
-  render() {
-    const role_id = Math.min(...document.roles.map(role => role.permission));
-    const routes = routesConfig[role_id];
+    render() {
+        const role_id = Math.min(...document.roles.map(role => role.permission));
+        const routes = routesConfig[role_id];
 
-    if (!routes) {
-      return <ComingSoon/>;
+        if (!routes) {
+            return <ComingSoon/>;
+        }
+
+        return (
+            <Router>
+                <div>
+                    <Header/>
+                    <Switch>
+                        {routes['routes'].map(route =>
+                            <Route key={route.path} exact path={route.path} component={route.component}/>
+                        )}
+                        <Route component={routes['defaultComponent']}/>
+                    </Switch>
+                </div>
+            </Router>
+        );
     }
-
-    return (
-      <Router>
-        <div>
-          <Header/>
-          <Switch>
-            {routes['routes'].map(route =>
-              <Route key={route.path} exact path={route.path} component={route.component}/>
-            )}
-            <Route component={routes['defaultComponent']}/>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
 }
 
 export default App;
