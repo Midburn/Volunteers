29f7452d89312bd2ca1a09c1b7c691c54347bc8d
shay <shay@intezer.com>
2018-02-11 12:34:18 +0200

    fix(volunteer) - spark info - modal valid profile attribute

--- src/components/VolunteerTab/VolunteerRequestPreviewModal.js
+++ src/components/VolunteerTab/VolunteerRequestPreviewModal.js
@@ -52,7 +52,7 @@ export default class VolunteerRequestPreviewModal extends React.Component {
   }
 
   errorMessage = _ => {
-    if (!this.state.request.validProfile) {
+    if (!this.state.request.sparkInfo.validProfile) {
       return (
         <Alert className="profile-alert" bsStyle="danger">
           <big><strong>{this.state.request.userId}</strong> isn't a valid Midburn Profile.</big><br/>
