37fdac65e01f696d466d083e71662394f7367060
Idan Cohen <icohen@salesforce.com>
2018-12-17 20:37:12 +0200

    change columns names

--- server/models/eventsPhases.js
+++ server/models/eventsPhases.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const eventsPhasesSchema = new Schema({
     _id: String,
     eventId: String,
-    phase1GrantingStart: Date,
-    phase1GrantingEnd: Date,
-    phase2PurchaseStart: Date,
-    phase2PurchaseEnd: Date,
+    phase1GrantingStartDate: Date,
+    phase1GrantingEndDate: Date,
+    phase2PurchaseStartDate: Date,
+    phase2PurchaseEndDate: Date,
 }, {_id: false, timestamps: {createdAt: 'createdAt', updatedAt: 'updatedAt'}});
 
 const eventsPhases = mongoose.model('EventsPhases', eventsPhasesSchema);
